95f539ac63c0d40cabbcba6af17252fa6e0aa756
Yurii Momotenko <momotenko.yuriy@gmail.com>
2021-08-30 18:42:36 +0300

    Removed copus

--- internal/service/main.go
+++ internal/service/main.go
@@ -5,14 +5,11 @@ import (
 	"net/http"
 
 	"github.com/Alien-Worlds/missions-api/internal/config"
-	"gitlab.com/distributed_lab/kit/copus/types"
 	"gitlab.com/distributed_lab/logan/v3"
-	"gitlab.com/distributed_lab/logan/v3/errors"
 )
 
 type service struct {
 	log      *logan.Entry
-	copus    types.Copus
 	listener net.Listener
 }
 
@@ -20,12 +17,6 @@ func (s *service) run(cfg config.Config) error {
 	s.log.Info("Running api service")
 
 	r := s.router(cfg)
-
-	if err := s.copus.RegisterChi(r); err != nil {
-		s.log.Info("errored while running api service")
-
-		return errors.Wrap(err, "cop failed")
-	}
 
 	return http.Serve(s.listener, r)
 }
@@ -35,7 +26,6 @@ func newService(cfg config.Config) *service {
 
 	return &service{
 		log:      log,
-		copus:    cfg.Copus(),
 		listener: cfg.Listener(),
 	}
 }
